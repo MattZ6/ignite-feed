3917fca0f8bdd7a4a80430b353f81d4a4da71b4e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-06-02 16:02:44 -0300

    feat(Sidebar): create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Header } from "./components";
+import { Header, Sidebar } from "./components";
 
 import './styles/global.css';
+
+import styles from './styles/App.module.css';
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './styles.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        src="https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/MattZ6.png?size=56" />
+
+        <strong>Matheus Zanin</strong>
+        <span>Sofware Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit profile
+        </a>
+      </footer>
+    </aside>
+  );
+}
