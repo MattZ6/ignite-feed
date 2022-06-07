f86a026f915425f7e1ab239c82da0acfb2b17675
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-06-07 11:22:56 -0300

    chore: add web manifest file

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,7 +7,8 @@ export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img
-        src="https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        src="https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt="Picture of a hill with trees"
       />
 
       <div className={styles.profile}>
